4215bdb66f78ff16b2610e5e73d01be0039b7b42
倪凯 <kai.ni@ushopal.com>
2019-09-26 15:45:44 +0800

    onlyCover

---
+++ docs/assets/index.js
@@ -0,0 +1,43 @@
+/*
+ * @version: 1.0.0
+ * @Date: 2019-09-26 14:51:10
+ * @LastEditTime: 2019-09-26 15:44:18
+ */
+
+'use strict';
+// 缓存
+if (typeof navigator.serviceWorker !== 'undefined') {
+    navigator.serviceWorker.register('sw.js')
+}
+
+window.$docsify = {
+    el: '#main',
+    homepage: 'home.md',
+    name: '个人文档',
+    nameLink: '#/home',
+    repo: 'https://github.com/nkHub',
+    loadNavbar: true,
+    coverpage: true,
+    onlyCover: true,
+    loadSidebar: true,
+    maxLevel: 3,
+    auto2top: true,
+    search: 'auto',
+    search: {
+        maxAge: 86400000,
+        path: 'auto',
+        placeholder: '搜索',
+        noData: '找不到结果!',
+        depth: 6
+    },
+    copyCode: {
+        buttonText: '复制到粘贴板',
+        errorText: '错误',
+        successText: '复制成功'
+    },
+    pagination: {
+        previousText: '上一章节',
+        nextText: '下一章节',
+        crossChapter: true
+    }
+}

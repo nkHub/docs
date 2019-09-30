046dedf69365ca2ab0b9900e6c960044fb9b7cd5
倪凯 <kai.ni@ushopal.com>
2019-09-30 11:47:21 +0800

    粒子效果添加

--- docs/assets/index.js
+++ docs/assets/index.js
@@ -79,6 +79,9 @@ window.$docsify = {
                         }
                     }, 0);
                 }
+                if (vm.route.path != '/') {
+                    initParticlesJS();
+                }
             });
         }
     ]
@@ -102,4 +105,118 @@ function formateDate(timestamp = Date.now(), formate = 'y-M-d h:m:s') {
         }
     }
     return formate;
+}
+
+// 初始化粒子背景
+function initParticlesJS() {
+    particlesJS('particles-js', {
+            "particles": {
+                "number": {
+                    "value": 70,
+                    "density": {
+                        "enable": true,
+                        "value_area": 1000
+                    }
+                },
+                "color": {
+                    "value": "random"
+                },
+                "shape": {
+                    "type": "circle",
+                    "stroke": {
+                        "width": 0,
+                        "color": "#ffffff"
+                    },
+                    "polygon": {
+                        "nb_sides": 5
+                    },
+                    "image": {
+                        "src": "./image/starburst_white_300_drop_2.png",
+                        "width": 100,
+                        "height": 100
+                    }
+                },
+                "opacity": {
+                    "value": 0.5,
+                    "random": false,
+                    "anim": {
+                        "enable": false,
+                        "speed": 1,
+                        "opacity_min": 0.1,
+                        "sync": false
+                    }
+                },
+                "size": {
+                    "value": 3,
+                    "random": true,
+                    "anim": {
+                        "enable": false,
+                        "speed": 40,
+                        "size_min": 0.1,
+                        "sync": false
+                    }
+                },
+                "line_linked": {
+                    "enable": true,
+                    "distance": 150,
+                    "color": "#aaaaaa",
+                    "opacity": 0.4,
+                    "width": 1
+                },
+                "move": {
+                    "enable": true,
+                    "speed": 3,
+                    "direction": "none",
+                    "random": false,
+                    "straight": false,
+                    "out_mode": "out",
+                    "attract": {
+                        "enable": false,
+                        "rotateX": 600,
+                        "rotateY": 1200
+                    }
+                }
+            },
+            "interactivity": {
+                "detect_on": "canvas",
+                "events": {
+                    "onhover": {
+                        "enable": false,
+                        "mode": "grab"
+                    },
+                    "onclick": {
+                        "enable": false,
+                        "mode": "push"
+                    },
+                    "resize": true
+                },
+                "modes": {
+                    "grab": {
+                        "distance": 150,
+                        "line_linked": {
+                            "opacity": 1
+                        }
+                    },
+                    "bubble": {
+                        "distance": 200,
+                        "size": 40,
+                        "duration": 2,
+                        "opacity": 8,
+                        "speed": 3
+                    },
+                    "repulse": {
+                        "distance": 200
+                    },
+                    "push": {
+                        "particles_nb": 1
+                    },
+                    "remove": {
+                        "particles_nb": 2
+                    }
+                }
+            },
+            "retina_detect": true
+        }
+
+    );
 }
--- docs/view/backend/nodejs/_sidebar.md
+++ docs/view/backend/nodejs/_sidebar.md
@@ -3,4 +3,5 @@
     * [Express](view/backend/nodeJs/express.md)
     * [Mongoose](view/backend/nodeJs/mongoose.md)
     * [Redis](view/backend/nodeJs/redis.md)
+    * [Nginx](view/backend/nodeJs/nginx.md)
 * [配置](view/config/config.md)
